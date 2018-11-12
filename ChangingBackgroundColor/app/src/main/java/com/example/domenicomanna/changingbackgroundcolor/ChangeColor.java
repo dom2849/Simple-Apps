c90680f4e62100faa6cf40952bce5a475b7a2c39
Domenico Manna <mannadomenico2849@gmail.com>
2018-11-11 19:25:23 -0500

    Add activity to change background color

---
+++ ChangingBackgroundColor/app/src/main/java/com/example/domenicomanna/changingbackgroundcolor/ChangeColor.java
@@ -0,0 +1,13 @@
+package com.example.domenicomanna.changingbackgroundcolor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangeColor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_color);
+    }
+}
