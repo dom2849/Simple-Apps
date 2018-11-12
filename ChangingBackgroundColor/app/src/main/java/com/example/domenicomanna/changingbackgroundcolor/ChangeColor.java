f6639ab8e9a050c3eb17ec3c0d5b90d29f719d8b
Domenico Manna <mannadomenico2849@gmail.com>
2018-11-11 19:43:03 -0500

    Add ability to switch activity

--- ChangingBackgroundColor/app/src/main/java/com/example/domenicomanna/changingbackgroundcolor/ChangeColor.java
+++ ChangingBackgroundColor/app/src/main/java/com/example/domenicomanna/changingbackgroundcolor/ChangeColor.java
@@ -1,13 +1,31 @@
 package com.example.domenicomanna.changingbackgroundcolor;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ChangeColor extends AppCompatActivity {
+    private static final String TAG = "ChangeColor";
+    private static final String CURRENT_COLOR_KEY = "Current Color";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_color);
+    }
+
+    /**
+     *
+     * @param packageContext the context being switched from
+     * @param currentBackgroundColor the current background color of the calling activity
+     * @return an intent that can be used to switch activities
+     */
+    public static Intent getIntentToSwitchActivity(Context packageContext, int currentBackgroundColor){
+        Log.d(TAG, "getIntentToSwitchActivity: Creating intent to switch activities");
+        Intent intent = new Intent(packageContext, ChangeColor.class);
+        intent.putExtra(CURRENT_COLOR_KEY, currentBackgroundColor);
+        return intent;
     }
 }
--- ChangingBackgroundColor/app/src/main/java/com/example/domenicomanna/changingbackgroundcolor/MainActivity.java
+++ ChangingBackgroundColor/app/src/main/java/com/example/domenicomanna/changingbackgroundcolor/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.domenicomanna.changingbackgroundcolor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private String CURRENT_COLOR_KEY = "Current Color";
+    private static final String CURRENT_COLOR_KEY = "Current Color";
     private int DEFAULT_COLOR = R.color.colorBlue;
 
     private LinearLayoutCompat linearLayout;
@@ -19,14 +21,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         currentBackgroundColor = DEFAULT_COLOR;
-
         if (savedInstanceState != null){
             Log.d(TAG, "onCreate: Restoring previous state");
             currentBackgroundColor = savedInstanceState.getInt(CURRENT_COLOR_KEY, DEFAULT_COLOR);
         }
-
         linearLayout = findViewById(R.id.linear_layout_compat_main);
+        changeColor = findViewById(R.id.btn_main_change_color);
+        changeColor.setOnClickListener((View v)-> switchToChangeColorActivity());
         changeBackGroundColor(currentBackgroundColor);
+
+    }
+
+    /**
+     * Switches to the change color activity
+     */
+    private void switchToChangeColorActivity() {
+        Intent intent = ChangeColor.getIntentToSwitchActivity(this, currentBackgroundColor);
+        startActivity(intent);
     }
 
     /**
